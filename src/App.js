a1c69a13dd6b4318798acb15395eadf8ddf39584
Tibo Keri <tiborkeri@icloud.com>
2018-07-26 16:47:35 +0100

    background fix

--- src/App.js
+++ src/App.js
@@ -1,90 +1,90 @@
 import React, { Component } from 'react';
-import './App.css';
 import Title from './component/Title';
-import Form from './component/Form'
-import Weather from './component/Weather'
-
-
+import Form from './component/Form';
+import Weather from './component/Weather';
+import './App.css';
+// import Forecast from './component/Forecast'
 
 const API_KEY = "e3a7c7ce72b0e8bcd9f70694cbfea8cf";
 
 class App extends Component {
-  state = {
-    temperature: null,
-    city: null,
-    country: null,
-    humidity: null,
-    description: null,
-    error: null,
-    isHidden: true,
-    curTime: null,
-    // image: null,
-  };
-  // custom method for API call
+  constructor(props) {
+    super();
+    this.state = {
+      temperature: null,
+      city: null,
+      country: null,
+      humidity: null,
+      description: null,
+      error: null,
+      showTitle: true,
+      curTime: null
+    };
+  }
 
-  getWeather = async e => {
-    console.log('async')
+  getWeather2 = e => {
     e.preventDefault();
-    // get name of input values
     const city = e.target.elements.city.value;
-    if (city) {
-      const api_call = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
-      );
-      const data = await api_call.json();
-      console.log(data)
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    )
+      .then(data => data.json())
+      .then(data => this.handleResponse(data, city))
+      .catch(err => console.log(err));
+    };
+    
+    handleResponse = (data, city) => {
       if (data.cod === 200) {
-        this.setState({
-          temperature: data.main.temp,
-          city: data.name,
-          humidity: data.main.humidity,
-          description: data.weather[0].description,
-          error: ""
-        }, this.getImages());
-      } else {
-        this.setState({
-          temperature: null,
-          city: null,
-          humidity: null,
-          description: null,
-          error: `There is no ${city} `
-        });
-      }
+        this.setState(
+          {
+            temperature: data.main.temp,
+            city: data.name,
+            humidity: data.main.humidity,
+            description: data.weather[0].description,
+            error: ""
+          },
+        this.ShowTitle()
+      );
     } else {
       this.setState({
         temperature: null,
         city: null,
         humidity: null,
         description: null,
-        image: null,
-        error: "Please enter a value!"
+        showTitle: false,
+        error: `There is no ${city} `
       });
     }
+    console.log(data)
   };
-  getImages(){
+
+  ShowTitle() {
     this.setState({
-      isHidden: false,
-    })
+      showTitle: false
+    });
   }
   componentWillMount() {
-    setInterval(function () {
-      this.setState({
-        curTime: new Date().toLocaleTimeString()
-      })
-    }.bind(this), 1000);
+    setInterval(
+      function() {
+        this.setState({
+          curTime: new Date().toLocaleTimeString()
+        });
+      }.bind(this),
+      1000
+    );
   }
-  
+
   render() {
     return (
       <div className="App">
-        {this.state.isHidden && <Title curTime={this.state.curTime}/>}
+        {this.state.showTitle && <Title curTime={this.state.curTime} />}
         <Weather
           temperature={this.state.temperature}
           city={this.state.city}
           humidity={this.state.humidity}
           error={this.state.error}
         />
-        <Form getWeather={this.getWeather} />
+        <Form getWeather={this.getWeather2} />
       </div>
     );
   }
--- src/component/Form.js
+++ src/component/Form.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Form = props => (
       <div className="form">
         <form onSubmit={props.getWeather}>
-         <h1> Enter a city  <span>name</span> </h1><hr/>
+          <h1> Enter a city  <span>name</span> </h1><hr/>
           <input className="rtl"type="text" name="city" placeholder="city" />
           <span >then hit enter</span>
         </form>
