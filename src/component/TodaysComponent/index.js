dc1a2fbb3f8d1c581e5560e3d9dc4133efd134ad
Tibo Keri <tiborkeri@icloud.com>
2018-08-15 22:50:39 +0100

    clean up TodaysComp

--- src/component/TodaysComponent/index.js
+++ src/component/TodaysComponent/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import './style.css'
 
 
-const TodaysComp = (props) => {
-
-    return (<div className="TodaysComp">
-        <p>in<span className="cityName"> {props.city}</span></p>
-        <p>the current temperature is <span className="temp">{props.temperature} &deg;C</span></p>
-        <p>with <span className="temp">{props.humidity}%</span>humidity.</p>
-        <p>{props.error}</p>
-</div>);
-  }
+const TodaysComp = (props) => (
+  <div className="TodaysComp">
+    <p>in<span className="cityName"> {props.city}</span></p>
+    <p>the current temperature is <span className="temp">{props.temperature} &deg;C</span></p>
+    <p>with <span className="temp">{props.humidity}%</span>humidity.</p>
+    <p>{props.error}</p>
+  </div>
+);
 
 export default TodaysComp;
