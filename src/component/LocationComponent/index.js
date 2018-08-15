b2b72cc49b4e004a99e08390c2f5383dc787449f
Tibo Keri <tiborkeri@icloud.com>
2018-08-15 17:16:52 +0100

    Toggle button styles

--- src/component/LocationComponent/index.js
+++ src/component/LocationComponent/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import './style.css'
 
+
 const LocationComp = props => (
 
-// 1 Date 
-// 2 Button - toggle permission for currentLoc
+
 
       <div className="LocationComp">
         <p> Enter a city  <span>name</span> </p><hr/>
         <form onSubmit={props.getWeather}>
           <input type="text" name="city" placeholder="city" />
         </form>
+        <button className="toggleButton" onClick={props.getPermission}></button>
+
       </div>
     )
-  
 
 export default LocationComp;
