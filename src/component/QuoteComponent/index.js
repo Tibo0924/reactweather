321a8b106bbf9febbd2a8c8be536e09312dfef79
Tibo Keri <tiborkeri@icloud.com>
2018-08-15 17:17:35 +0100

    Quote component  from now renders random quotes

--- src/component/QuoteComponent/index.js
+++ src/component/QuoteComponent/index.js
@@ -2,12 +2,63 @@ import React from 'react' ;
 import './style.css'
 
 
-const QuoteComp = (props) => (
-      <div className="QuoteComp">
-        <h1>City Weather <span>finder</span></h1>
-        <p className="time">{props.curTime}</p>
-        <p>Find out the most recent weather conditions of your city.</p>
-      </div>
-    )
 
+class QuoteComp extends React.Component {
+  constructor(props) {
+    super(props) 
+    this.state = {
+      curTime: null,  
+      quote: null,
+
+    }
+  }
+
+
+
+  
+
+  // handleQuote
+  handleQuote = (data) => {
+    console.log(data)
+    this.setState({
+      quote: data[0].content,
+    })
+  }
+  // APi Call for  quotes
+  componentDidMount() {
+    fetch (`
+        https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1`
+    )
+    .then(data => data.json())
+    .then(data => this.handleQuote(data))
+
+  }
+  
+
+  componentWillMount() {
+    setInterval(
+      function() {
+        this.setState({
+          curTime: new Date().toLocaleTimeString()
+        });
+      }.bind(this),
+      1000
+    );
+  }
+
+
+render() {
+  return ( 
+      <div className="QuoteComponent">
+        <h2>{this.state.curTime}</h2>
+        <div 
+          className="quoteContainer" 
+          dangerouslySetInnerHTML={{__html: this.state.quote}}
+          >
+        </div>
+      </div>
+  ) }
+
+  
+}
 export default QuoteComp;
